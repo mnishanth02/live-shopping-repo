600754af081c76cc338f69d5e173b469a5cadd0d
nishanth Murugan <nishanth.murugan@gmail.comn>
2020-07-11 13:01:53 +0530

    upload Size increase

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ const ownerRoutes = require("./router/shopRouter");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 // app.use("/images", express.static(path.join("images")));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // app.use(passport.initialize())
 
 // passport.use(passportMiddleWare)
